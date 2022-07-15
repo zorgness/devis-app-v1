078546c7206367f8769f904e98d467c8887cbd56
Thomas Fouquert <tfouquert@gmail.com>
2022-07-15 17:22:13 +0200

    add_new_quotation

--- app/controllers/quotations_controller.rb
+++ app/controllers/quotations_controller.rb
@@ -6,4 +6,25 @@ class QuotationsController < ApplicationController
   def show
     @quotation = Quotation.find(params[:id])
   end
+
+  def new
+    @customers = Customer.where(user_id: current_user.id)
+    @quotation = Quotation.new
+  end
+
+  def create
+    @quotation = Quotation.new(quotation_params)
+    @quotation.user = current_user
+    if @quotation.save
+      redirect_to @quotation
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def quotation_params
+    params.require(:quotation).permit(:number, :date, :customer_id, :total_price)
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,5 @@
 class Customer < ApplicationRecord
   belongs_to :user
   has_many :quotations
+
 end
