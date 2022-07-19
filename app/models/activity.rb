b13260ef2f7eb623cb223a9b36f96f80433f30da
Thomas Fouquert <tfouquert@gmail.com>
2022-07-19 17:52:46 +0200

    search_for_quotations_and_customers

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,5 @@
 class Activity < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  belongs_to :user
 end
--- app/models/element.rb
+++ app/models/element.rb
@@ -1,4 +1,5 @@
 class Element < ApplicationRecord
   has_many :tasks, dependent: :destroy
   validates :name, presence: true, uniqueness: true
+  belongs_to :user
 end
